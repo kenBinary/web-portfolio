8f0ebfb1ba53f5f3f8d3dcef2590dd83708348c8
thennek0 <kennethbecaro0@gmail.com>
2023-12-17 23:24:50 +0800

    add icon and dynamic education card content

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -1,14 +1,26 @@
 import '../styles/education-card.css';
-export default function EducationCard({ alignment }) {
+import universityIcon from '/school-building.svg';
+import degreeIcon from '/graduate-cap.svg';
+import addressIcon from '/map-marker.svg';
+export default function EducationCard({ alignment, university, degree, address }) {
     function contentAlignment(alignment) {
         return (alignment === 'start') ? 'education-card-start' : 'education-card-end';
     }
     return (
         <div className={'education-card ' + contentAlignment(alignment)}>
             <div>
-                <p id='university'>University Of Mindanao (2021-2025)</p>
-                <p id='degree'>Bachelor Of Science In Computer Science</p>
-                <p id='address'>location icon: Davao City, Philippines</p>
+                <p id='university'>
+                    <img src={universityIcon} alt="" />
+                    {university}
+                </p>
+                <p id='degree'>
+                    <img src={degreeIcon} alt="" />
+                    {degree}
+                </p>
+                <p id='address'>
+                    <img src={addressIcon} alt="" />
+                    {address}
+                </p>
             </div>
         </div>
     );
--- src/pages/EducationPage.jsx
+++ src/pages/EducationPage.jsx
@@ -1,14 +1,23 @@
 import EducationCard from '../components/EducationCard';
 import '../styles/education-page.css';
 export default function EducationPage() {
+
     return (
         <section className="education-section">
             <h1>Education</h1>
             <div className='education'>
-                <EducationCard alignment={'start'}></EducationCard>
-                <EducationCard alignment={'end'}></EducationCard>
-                <EducationCard alignment={'start'}></EducationCard>
-                <EducationCard alignment={'end'}></EducationCard>
+                <EducationCard
+                    alignment={'start'}
+                    university={'University Of Mindanao (2021-2025)'}
+                    degree={'Bachelor of Science in Computer Science'}
+                    address={'Davao City, Philippines'}
+                ></EducationCard>
+                <EducationCard
+                    alignment={'end'}
+                    university={'STI malaybalay (2019-2021)'}
+                    degree={'IT in mobile app and web development'}
+                    address={'Malaybalay City, Philippines'}
+                ></EducationCard>
             </div>
         </section>
     );
