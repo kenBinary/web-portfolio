40b7a19d24030010079f85d302be3b3f5d294f39
thennek0 <kennethbecaro0@gmail.com>
2023-12-17 15:05:37 +0800

    add contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from './components/NavBar';
 import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 import EducationPage from './pages/EducationPage';
 import ProjectPage from './pages/ProjectPage';
 import SkillsPage from './pages/SkillsPage';
@@ -12,6 +13,7 @@ function App() {
       <SkillsPage></SkillsPage>
       <ProjectPage></ProjectPage>
       <EducationPage></EducationPage>
+      <ContactPage></ContactPage>
     </>
   );
 }
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,12 @@
+import '../styles/contact-page.css';
+export default function ContactPage() {
+    return (
+        <section className="contact-page">
+            <h1>Contact</h1>
+            <p>Feel free to contact me directly on <span>test@gmail.com</span></p>
+            <div className='copyright'>
+                2023 - Made by kenneth joshua becaro
+            </div>
+        </section>
+    );
+}
